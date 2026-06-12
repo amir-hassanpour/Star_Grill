3862591c4102b01a9ebf396663d96bfc74e595fd
Amir Hassanpour <amirhassanpour@Amirs-MacBook-Pro-2.local>
2026-06-12 15:01:30 -0400

    ssytem commands

--- src/main/java/Star_Grill_POS.java
+++ src/main/java/Star_Grill_POS.java
@@ -1,5 +1,14 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import javax.print.Doc;
+import javax.print.DocFlavor;
+import javax.print.DocPrintJob;
+import javax.print.PrintException;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.SimpleDoc;
 
 class CustomerNumber {
     private int customerNumber;
@@ -97,10 +106,49 @@ class Payments {
     }
 }
 
-class cashOpener {
+class CashDrawerOpener {
+    public static void cashDrawerOpener() {
+        PrintService printer = PrintServiceLookup.lookupDefaultPrintService();
 
+        if (printer == null) {
+            return;
+        }
+
+        byte[] openDrawerCommand = new byte[] {
+                0x1B, 0x40,
+                0x1B, 0x70, 0x00, 0x64, (byte) 0xFF
+        };
+
+        try {
+            DocPrintJob job = printer.createPrintJob();
+
+            Doc doc = new SimpleDoc(openDrawerCommand, DocFlavor.BYTE_ARRAY.AUTOSENSE, null);
+            job.print(doc, null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
-class print {
+class Print {
+    public static void print(String fileName) {
+        try {
+            PrintService printer = PrintServiceLookup.lookupDefaultPrintService();
 
+            if (printer == null) {
+                return;
+            }
+
+            byte[] fileBytes = Files.readAllBytes(Path.of(fileName));
+            DocPrintJob job = printer.createPrintJob();
+
+            Doc doc = new SimpleDoc(fileBytes, DocFlavor.BYTE_ARRAY.AUTOSENSE, null);
+            job.print(doc, null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
 }
---
+++ src/main/java/Star_Grill_Text_File_Editor.java
@@ -0,0 +1,2 @@
+public class Star_Grill_Text_File_Editor {
+}
